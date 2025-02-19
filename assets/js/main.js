552bd776bd6da0cdbf44b841a7590b3d62909a87
FreshPotPie <getagrip321@gmail.com>
2025-02-18 19:23:31 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -203,6 +203,11 @@ $forward.show();
 $backward.show();
 
 }
+  var frmvalidator  = new Validator("contactform");
+    frmvalidator.addValidation("name","req","Please provide your name");
+    frmvalidator.addValidation("email","req","Please provide your email");
+    frmvalidator.addValidation("email","email",
+      "Please enter a valid email address");
 
 $t._update();
 
